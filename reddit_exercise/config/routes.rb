aaa71e43d6c48d336d0de11ac55daf44a4145e3d
thedaebu <min.kim163@gmail.com>
2021-02-15 14:56:41 -0500

    started user show erb

--- reddit_exercise/config/routes.rb
+++ reddit_exercise/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resources :subs, only: [:create]
+    resources :posts, only: [:create]
+  end
   resource :session
-  resources :subs, except: [:destroy]
-  resources :posts, except: [:index]
+  resources :subs, except: [:destroy, :create]
+  resources :posts, except: [:index, :create]
 
 end
