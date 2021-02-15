f6b6042c07453b64f061f285af45d9005b2c2ad2
thedaebu <min.kim163@gmail.com>
2021-02-15 10:35:21 -0500

    finished application controller

---
+++ reddit_exercise/app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+    
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def generate_session_token!
+        SecureRandom::base64
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token!
+        self.save!
+        self.session_token
+
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= generate_session_token!
+    end
+end
