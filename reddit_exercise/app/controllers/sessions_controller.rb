70e8159332555c14f3ea9df0fe7c11838f2ff2db
Ariton Sefedini <aritonsefedini@icloud.com>
2021-02-15 10:52:19 -0500

    Created models and controllers

--- reddit_exercise/app/controllers/sessions_controller.rb
+++ reddit_exercise/app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    before_action :ensure_logged_in?, only: [:destroy]
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            log_in!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = ["Invalid credentials"]
+            render :new 
+        end
+    end
+
+    def destroy
+        log_out!
+        redirect_to new_session_url
+    end
+
 end
--- reddit_exercise/app/controllers/users_controller.rb
+++ reddit_exercise/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+    before_action :ensure_logged_in?, only: [:index, :show]
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
